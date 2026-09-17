0ce9b5d43dbe82bac585dcf517a4233717b787ff
lJesusl-f <pedroenriquejacarei@gmail.com>
2026-09-17 17:50:32 -0300

    Portfolio 4dsm

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,10 @@
 // Seleciona o botão pelo ID
 const changeBgButton = document.getElementById('changeBgButton');
 
-// Função que alterna a classe background-changed no body
+// Função que alterna a classe background-changed no body e no html
+// (o html precisa da classe também para que o fundo mude até nas
+// bordas da tela, em monitores mais largos que o conteúdo)
 changeBgButton.addEventListener('click', function() {
   document.body.classList.toggle('background-changed');
+  document.documentElement.classList.toggle('background-changed');
 });
